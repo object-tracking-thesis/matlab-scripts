e481f2c613fc125f5357899e1d3ad99c89da53a6
nilmagnum <leue@student.chalmers.se>
2016-02-12 10:51:56 +0100

    return xy diff between two gps positions

---
+++ matlabPCapi/gpsDiff2xyDiff.m
@@ -0,0 +1,28 @@
+function xyDiff = gpsDiff2xyDiff(gpsOld, gpsNew)
+% returns the x and y diff in meters between two gps positions
+% in:
+%   gpsOld - gps Position to compare with, 1x2 vector, [lat lon] in deg
+%   gpsNew - current gps Position, 1x2 vector, [lat lon] in deg
+% out:
+%   xyDiff - difference between the two positions, 1x2 vector, [x y] in m
+
+gpsOld = deg2rad(gpsOld);
+gpsNew = deg2rad(gpsNew);
+gpsDiff = gpsNew - gpsOld;
+
+radius=6371;
+
+%pythagorean distance in meters
+x=gpsDiff(2)*cos((gpsOld(1)+gpsNew(1))/2)*radius*1000;
+y=gpsDiff(1)*radius*1000;
+d=sqrt(x*x + y*y);
+xyDiff = [x y];
+
+end
+
+% Haversine distance is the distance along the earth's surface and not the
+% straight distance, but this is literally the same for these short diffs
+% a=sin((gpsDiff(1))/2)^2 + cos(gpsOld(1))*cos(gpsNew(1)) * sin(gpsDiff(2)/2)^2;
+% c=2*atan2(sqrt(a),sqrt(1-a));
+% d1km=radius*c;
+% meterDiff(1) = d1km*1000;
