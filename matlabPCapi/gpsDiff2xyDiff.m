53bdbe2e41de0519517dafdc892925a4aedc4ad3
nilmagnum <leue@student.chalmers.se>
2016-02-15 14:31:20 +0100

    changed radius of the earth (muahahahahaha)

--- matlabPCapi/gpsDiff2xyDiff.m
+++ matlabPCapi/gpsDiff2xyDiff.m
@@ -10,12 +10,12 @@ gpsOld = deg2rad(gpsOld);
 gpsNew = deg2rad(gpsNew);
 gpsDiff = gpsNew - gpsOld;
 
-radius=6371;
+radius=6378.1;
 
 %pythagorean distance in meters
 y=-gpsDiff(2)*cos((gpsOld(1)+gpsNew(1))/2)*radius*1000;
 x=gpsDiff(1)*radius*1000;
-%d=sqrt(x*x + y*y);
+%d=sqrt(x*x + y*y)
 
 xyDiff = [x y];
 
