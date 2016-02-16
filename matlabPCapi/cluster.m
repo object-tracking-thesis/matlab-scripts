07dd7a87ea32dcf9a52b35e852d96895886989f0
nilmagnum <leue@student.chalmers.se>
2016-02-16 09:52:43 +0100

    clustering skeleton function

---
+++ matlabPCapi/cluster.m
@@ -0,0 +1,11 @@
+function pcdClusters = cluster(pcdXYZ, d)
+% returns clusters of points that can be reached via length d edges
+% in:
+%   pcdXYZ  - point cloud data in XYZ
+%   d       - max. distance to be considered a relevant neighbor
+% out:
+%   pcdClusters - clusters of point cloud data in XYZ
+
+
+
+end
