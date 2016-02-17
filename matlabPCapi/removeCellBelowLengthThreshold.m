549077c4fc0e459ecb4952ac4d805f8e7285081f
Marko Cotra <markocotra@Markos-MacBook-Pro.local>
2016-02-17 09:36:19 +0100

    changes

--- matlabPCapi/removeCellBelowLengthThreshold.m
+++ matlabPCapi/removeCellBelowLengthThreshold.m
@@ -6,13 +6,23 @@ function cellArray = removeCellBelowLengthThreshold(cellArray, threshold)
 %   cellArray - array of cells with cells below a length threshold
 %                      removed
 
-i = 1;
-while i < length(cellArray)
-    if length(cellArray{i}) < threshold
-        cellArray(i) = [];
-    else
-        i = i+1;
-    end
-end
+% i = 1;
+% while i < length(cellArray)
+%     if length(cellArray{i}) < threshold
+%         cellArray(i) = [];
+%     else
+%         i = i+1;
+%     end
+% end
+
+num = 1:length(cellArray);
+A = cellfun('length',cellArray);
+smaller = A < threshold;
+
+remove = smaller.*num';
+remove = remove(remove > 0);
+
+cellArray(remove) = [];
+
 
 end
