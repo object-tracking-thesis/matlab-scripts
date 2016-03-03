b1127f5751577406ef6ed5b1e28b3874938ff5ae
nilmagnum <leue@student.chalmers.se>
2016-03-03 09:22:35 +0100

    some refactoring to include [lat, lon, alt] in one argument vector

--- matlabPCapi/translateFrame.m
+++ matlabPCapi/translateFrame.m
@@ -1,17 +1,15 @@
-function translatedFrame = translateFrame(pcdXYZ, gpsStatic, gpsLive, altStatic, altLive)
+function translatedFrame = translateFrame(pcdXYZ, gpsStatic, gpsLive)
 % translates the current live frame in XYZ in relation to a static environment 
 % according to a difference between static/live GPS and static/live altitude
 % in:
 %   pcdXYZ -        Nx3 matrix, [x, y, z]
-%   gpsStatic -     1x2 matrix, old frame position, [lat, lon] deg
-%   gpsLive -       1x2 matrix new frame position, [lat, lon] deg
-%   altStatic -     scalar, old altitude, meter
-%   altLive -       scalar, new altitude, meter
+%   gpsStatic -     1x3 vector, old frame position, [lat, lon, alt] deg,deg,m
+%   gpsLive -       1x3 vector new frame position, [lat, lon, alt] deg,deg,m
 % out:
 %   translatedFrame -  Nx3 matrix, [x, y, z]
 
-xyDiff = gpsDiff2xyDiff(gpsStatic, gpsLive);
-zDiff = altLive - altStatic;
+xyDiff = gpsDiff2xyDiff(gpsLive(1:2), gpsStatic(1:2));
+zDiff = gpsLive(3) - gpsStatic(3);
 xyzDiff = [xyDiff zDiff]; %1x3
 
 %translate for x, y and z
