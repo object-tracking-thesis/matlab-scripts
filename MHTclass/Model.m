5327b335e17890c3d301a9d1c7d53bc5f9d789f4
Marko Cotra <cotra.marko@gmail.com>
2016-03-08 17:13:20 +0100

    Extended to account for new target initiation, clutter and detection probabilities

--- MHTclass/Model.m
+++ MHTclass/Model.m
@@ -1,8 +1,18 @@
 classdef Model
-    properties (Constant)
-        A = 1;
-        H = 1;
-        Q = 1;
-        R = 1;
+    properties (Constant) 
+        % Used for updating posterior 
+        A = 1; % Transition Matrix 
+        H = 1; % Measurement Matrix 
+        Q = 1; % Covariance for Transition
+        R = 1; % Covariance for Measurement 
+        
+        % Used for track initiation
+        vmax = 4;  % Maximal (assumed) velocity
+        kappa = 3; % scaling factor 
+        
+        % Contains detection probability and clutter specification 
+        rho = 0.1; % False Measurement Density 
+        V = 1;     % Volume/Area of Measurement Space
+        Pd = 1;    % Detection Probability 
     end
 end
