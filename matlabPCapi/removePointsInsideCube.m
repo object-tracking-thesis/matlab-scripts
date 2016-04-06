74715d866da87d6cc3a5801b424b883a20d501bb
nilmagnum <leue@student.chalmers.se>
2016-04-06 14:36:59 +0200

    vectorized the point removal routine, ~10x faster now

--- matlabPCapi/removePointsInsideCube.m
+++ matlabPCapi/removePointsInsideCube.m
@@ -1,4 +1,4 @@
-function remainingPoints = removePointsInsideCube(cube, points)
+function points = removePointsInsideCube(cube, points)
 %return whether a point is inside a cube or not
 %in:
 %cube - 9x1 vector, x,y,z(center), length,width,height, roll,pitch,yaw 
@@ -6,10 +6,9 @@ function remainingPoints = removePointsInsideCube(cube, points)
 %out:
 %remaining - nx3 vector, x,y,z the remaining points not inside the cube
 
-remainingPoints = zeros(60000,3);
-
-%TODO why -2 in the z direction?
-[px, py, pz] = cubePlanes(cube(1:3)',cube(4),cube(5),cube(6),cube(7:9)',-1.5,cube(3)-2.5);
+%TODO why -4 in the z direction?
+%cube = cube'
+[px, py, pz] = cubePlanes(cube(1:3)',cube(4),cube(5),cube(6),cube(7:9)',-2,cube(3)-4);
 nFaces = size(px,2);
 origin = repmat(cube(1:3)',1,nFaces);
 
@@ -22,29 +21,27 @@ p13 = [px(3,j); py(3,j); pz(3,j)];
 %normal vectors for each of the faces of the cube
 normalVectors = cross(p12-p11,p13-p11);
 
-%calculate distance for the center point of the cube from all 6 faces and
-%normalize it
+%calculate distance for the center point of the cube from all 6 faces,
+%normalize it and build a repeat matrix over the number of all points
+%(6x6 matrix for each point, row repeated)
 od = (origin-p11)'*normalVectors;
 od = od./abs(od);
+od = repmat(od,size(points,1),1);
 
-for i=1:size(points,1)
-    point = repmat(points(i,:)',1,nFaces);
-    %calculate distance for the test point from all 6 faces and normalize it
-    pd = (point-p11)'*normalVectors;
-    pd = pd./abs(pd);
-    
-    %both points have to lie on the same side of each of the 6 faces of the
-    %cube, otherwise the point lies outside the cube
-    t = pd-od;
-    
-    %..if the test matrix is not all zeros, then the point and the cube origin
-    %are on different sides of at least one face and therefore the point
-    %lies outside of the cube, therefore keep...
-    if ~(all(t(:) == 0))
-        remainingPoints(i,:) = points(i,:);
-    end
-end
+%calculate distance for a repeat matrix of all points and normalize it
+%(6x6 matrix for each point, row repeated)
+pointMat = repelem(points',1,nFaces);
+pd = (pointMat-repmat(p11,1,size(points,1)))'*normalVectors;
+pd = pd./abs(pd);
 
-remainingPoints(all(remainingPoints == 0, 2),:) = [];
+%if pd and od are equal then both points have to lie on the same side of 
+%each of the 6 faces of the cube, otherwise the point lies outside the cube
+%subtract od from pd and sum up the 6x6 submatrices, t is finally a vector
+%of length(points) that is equal to zero if the point lies within the cube
+t = pd-od;
+t = abs(t);
+t = sum(t,2);
+t = sum(vec2mat(t,6),2);
 
-%remainingPoints = points;
+%remove all points that are within the cube
+points(t==0,:) = [];
