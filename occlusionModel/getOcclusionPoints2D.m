024974acf46d35a1beca8b9d5bc70ccec3be6400
Marko Cotra <cotra.marko@gmail.com>
2016-04-12 14:21:28 +0200

    Updates for testing and making getDetectionProbability() work

--- occlusionModel/getOcclusionPoints2D.m
+++ occlusionModel/getOcclusionPoints2D.m
@@ -1,27 +1,38 @@
 function idx = getOcclusionPoints2D(pointCloud)
-    % pointCloud = [x1, y1;...
-    %               x2, y2;...]
+    % The functions returns the index of the two points in a pointcloud 
+    % that span the occlusion-cone with respect to origin. 
+    % pointCloud is a N-by-2 matrix, where each row is a data point and the
+    % column values are x and y coordinates respectively. idx is a 1x2
+    % matrix where each element is an index for a row in pointCloud.
+    % 
+    % idx = getOcclusionPoints2D(pointCloud)
+    %              
 
+    % Get the geometric center point of the pointCloud
     m = mean(pointCloud);
     % Define perpendicular line
     perpLine = [1 -m(1)/m(2)]./norm([1 -m(1)/m(2)],2);
 
-    % project points onto perp line
-    projPoints = zeros(size(pointCloud));
-
-    for k = 1:size(projPoints,1)
-        projPoints(k,:) = sum(pointCloud(k,:).*perpLine) / sum(perpLine.*perpLine) .* perpLine;
-    end
-
+    % project points onto perp line, using the formula proj_s-on-v =
+    % s*dot(v,s)/dot(s,s)
+        
+    [r,c ] = size(pointCloud);
+    
+    top = sum(pointCloud .* repmat(perpLine,r,1),2);
+    bot = sum(perpLine.*perpLine);
+    
+    scalingFactor = top./bot;
+    projPoints = repmat(scalingFactor,1,c) .* repmat(perpLine,r,1);
+    
     % Get angle for PC1 & PC2
     x = [0 1];
     phi = acos(sum(perpLine.*x)/(norm(x,2)*norm(perpLine,2)));    
     % Rotate PC1 points by phi
-    R = [cos(phi) -sin(phi); sin(phi) cos(phi)];
-    
+    R = [cos(phi) -sin(phi); sin(phi) cos(phi)];    
 
     projPointsRotated = projPoints*R';
     
+    % From the rotated points, get the index for max and min value 
     [~, iMax] = max(projPointsRotated(:,2));
     [~, iMin] = min(projPointsRotated(:,2));
     
--- occlusionModel/occlusionTestModel.m
+++ occlusionModel/occlusionTestModel.m
@@ -1,101 +1,91 @@
-% Test to write occlusion model 
-rng(1337)
-% Generate random 2D point clouds 
+% Test to write occlusion model
+
+% Generate random 2D point clouds
 N = 15;
-PC1 = mvnrnd([4 3]', 0.1.*[1 0.2;0.2 1],N);
 
-PC2 = mvnrnd([-2 2]', 0.1.*[1 0.2;0.2 1],N);
+Q  = 0.01.*[1 0.2;0.2 1];
+a = -3;
+b = 3;
 
+
+K = 10;
+PC = cell(1,K);
+rng(1335)
+
+for k = 1:K
+    N = randi(100);
+    r = a*ones(2,1) + (b-a).*rand(2,1);
+    PC{k} = mvnrnd(r, Q,N);
+end
+close all
+
+pHandle = zeros(1,K);
+strings = cell(1,K);
+Pd = ones(K,1);
+%%
 figure('Position',[600 100 1000 800])
+
+PC{1} = PC{1} + repmat(mvnrnd([-1 -2],Q,1),length(PC{1}),1);
+for H = 1:1
+    clf
+    for k = 1:K
+        PC{k} = PC{k} + repmat(mvnrnd([0 0],Q,1),length(PC{k}),1);
+    end
+
+    Pd = getDetectionProbability(PC);
     hold on
     p1 = plot(0,0,'ko','MarkerFaceColor','k');
     legend(p1,'EGO Vehicle')
-    % PC1
-    plot(PC1(:,1), PC1(:,2),'.','MarkerSize',20)
-    m1 = mean(PC1);
-    plot(m1(1),m1(2),'bx','MarkerSize',12)
-    %plot([0 m1(1)], [0 m1(2)],'b')
-    % define perpendicular line
-    A1 = [1 -m1(1)/m1(2)]./norm([1 -m1(1)/m1(2)],2);    
-    
-    %plot([m1(1) m1(1)+A1(1)], [m1(2) m1(2)+A1(2)],'b')    
-   
-    % PC2
-    plot(PC2(:,1), PC2(:,2),'.','MarkerSize',20)        
-    m2 = mean(PC2);
-    plot(m2(1),m2(2),'rx','MarkerSize',12)  
-    %plot([0 m2(1)], [0 m2(2)],'r')
-    % define perpendicular line         
-    A2 = [1 -m2(1)/m2(2)]./norm([1 -m2(1)/m2(2)],2);
+    for k = 1:K
         
-    %plot([m2(1) m2(1)+A2(1)], [m2(2) m2(2)+A2(2)],'r')      
-
-    axis(0.5*[-10 10 -10 10])
-    
-    
-% project points from PC2 & PC1 onto perp line
-proj1 = zeros(size(PC1));
-proj2 = zeros(size(PC2));
-
-for k = 1:size(proj1,1)
-    proj1(k,:) = sum(PC1(k,:).*A1) / sum(A1.*A1) .* A1;
+        plot(PC{k}(:,1),PC{k}(:,2),'.','MarkerSize',20)
+        idx = getOcclusionPoints2D(PC{k});
+        plot([0 PC{k}(idx(1),1)],[0 PC{k}(idx(1),2)],'k')
+        plot([0 PC{k}(idx(2),1)],[0 PC{k}(idx(2),2)],'k')
+        plot(PC{k}(:,1),PC{k}(:,2),'.','MarkerSize',20,'MarkerEdgeColor',[1 1 1]-repmat(Pd(k),1,3));
+        strings{k} = sprintf('PointCloud %d Pd: %.2f',[k Pd(k)]);
+        m = mean(PC{k});
+        text(m(1)*1.1,m(2)*1.1,strings{k})
+    end
+    axis([-5 5 -5 5])
+    pause(1)
+    %waitforbuttonpress
 end
 
-for k = 1:size(proj2,1)
-    proj2(k,:) = sum(PC2(k,:).*A2) / sum(A2.*A2) .* A2;
-end
 
-proj1 = repmat(m1,size(proj1,1),1) + proj1;
-%plot(proj1(:,1), proj1(:,2),'db')
+%% Test out funcitons
 
-proj2 = repmat(m2,size(proj2,1),1) + proj2;
-%plot(proj2(:,1), proj2(:,2),'dr')
+x = 0:0.01:30;
+a = 0;
+b = 15;
+slope = 0.8;
+lowerLimit = 0.05;
 
-% Rotate points to Y, so that we can find max & min index 
+y1 = lowerLimit/2 + 0.9./exp(slope.*(x-a));
+y2 = lowerLimit/2 + 0.9./exp(-slope.*(x-b));
 
-% Get angle for PC1 & PC2
-x = [0 1];
-phi1 = acos(sum(A1.*x)/(norm(x,2)*norm(A1,2)));
-phi2 = acos(sum(A2.*x)/(norm(x,2)*norm(A2,2)));
-% Rotate PC1 points by phi
-R1 = [cos(phi1) -sin(phi1); sin(phi1) cos(phi1)];
+%plot(x,y1,x,y2)
+hold on
 
-R2 = [cos(phi2) -sin(phi2); sin(phi2) cos(phi2)];
+plot(x,y1+y2,'k')
 
-proj1rot = proj1*R1';
-proj2rot = proj2*R2';
-% plot rotated points 
-%plot(proj1rot(:,1), proj1rot(:,2),'db')
-%hold on
-%plot(proj2rot(:,1), proj2rot(:,2),'dr')
+axis([0 20 0 1])
 
-% Find min max points 
+%%
 
-%[~, i1Max] = max(proj1rot(:,2))
-%[~, i1Min] = min(proj1rot(:,2))
+a = 0;
+b = 10;
 
+I = mat2gray([a b])
 
-%[~, i2Max] = max(proj2rot(:,2))
-%[~, i2Min] = min(proj2rot(:,2))
+alpha = 2;
+L = 0.1;
+x = 0:0.1:10;
+f = @(x) L + (1-L)*(exp(-alpha*(x-a)) + exp(alpha*(x-b)));
+y = f(x)
 
-% plot(10.*[0 PC1(i1Max,1)],10.*[0 PC1(i1Max,2)],'m')
-% plot(10.*[0 PC1(i1Min,1)],10.*[0 PC1(i1Min,2)],'m')
-% 
-% plot(10.*[0 PC2(i2Max,1)],10.*[0 PC2(i2Max,2)],'m')
-% plot(10.*[0 PC2(i2Min,1)],10.*[0 PC2(i2Min,2)],'m')
-
-idx = getOcclusionPoints2D(PC1);
-plot(10.*[0 PC1(idx(1),1)],10.*[0 PC1(idx(1),2)],'m')
-plot(10.*[0 PC1(idx(2),1)],10.*[0 PC1(idx(2),2)],'m')
-
-idx = getOcclusionPoints2D(PC2);
-plot(10.*[0 PC2(idx(1),1)],10.*[0 PC2(idx(1),2)],'m')
-plot(10.*[0 PC2(idx(2),1)],10.*[0 PC2(idx(2),2)],'m')
-
-
-
-
-
+plot(x,y)
+axis([0 10 0 1])
 
 
 
