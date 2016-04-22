c7b4e71b48dcb42a4ec09152f2ba921faa7818f6
Marko Cotra <cotra.marko@gmail.com>
2016-04-22 17:02:12 +0200

    Added dynamic gaussian assignment and investigated rotation

--- modelingExtended.m
+++ modelingExtended.m
@@ -1,45 +1,171 @@
-% Look at modelling the density of an extended target 
-
+% Look at modelling the density of an extended target
+figure('Position',[600 250 1000 800])
 L = 4.58; % meters
 B = 1.80; % meters
- hold on
-s1 = [0 0];
-P1 = 0.01*[0.1 0;
-      0 1];
-N = 200;
-[X1,X2] = meshgrid(linspace(-2,8,N)', linspace(-2,8,N)');
-X = [X1(:) X2(:)];
-p = mvnpdf(X, s1, P1);
-sups = surf(X1,X2,reshape(p,N,N));
-sups.FaceAlpha = 0.2;
-sups.EdgeAlpha = 0.2;
+%
 
-s1 = [0 L];
-P1 = 0.01*[0.1 0;
-      0 1];
-[X1,X2] = meshgrid(linspace(-2,8,N)', linspace(-2,8,N)');
-X = [X1(:) X2(:)];
-p = mvnpdf(X, s1, P1);
-sups = surf(X1,X2,reshape(p,N,N));
-sups.FaceAlpha = 0.2;
-sups.EdgeAlpha = 0.2;
+
+means = zeros(2,4);
+covs  = zeros(2,2,4);
+
+% Place corner gaussians
+
+sbl = [0 0]'; % L bottom left corner
+Pbl = 0.01*[0.1 0;
+    -0.1 0.1];
+
+sbr = [B 0]'; % L bottom right corner
+Pbr = 0.01*[0.1 0.1;
+    0   0.1];
+
+sul = [0 L]'; % L upper left corner
+Pul = 0.01*[0.1 0.1;
+    0   0.1];
+
+sur = [B L]'; % L upper right corner
+Pur = 0.01*[0.1 0;
+    -0.1 0.1];
+
+means(:,1) = sbl;
+means(:,2) = sbr;
+means(:,3) = sul;
+means(:,4) = sur;
+
+covs(:,:,1) = Pbl;
+covs(:,:,2) = Pbr;
+covs(:,:,3) = Pul;
+covs(:,:,4) = Pur;
+
+hold on
+n = 100; % Number of grid points
+phi = linspace(0,2*pi,100); % Create grid in interval [0,2*pi] 4
+% Sigma ellipse
+Col = ['r', 'm','g'];
+
+for j = 1:length(means)
+    for sig = 1:3
+        x = repmat(means(:,j),1,n)+sig*sqrtm(covs(:,:,j))*[cos(phi);sin(phi)]; % Equation(12) in HA1 document
+        plot(x(1,:),x(2,:),Col(sig),'LineWidth',1)
+    end
+end
+K = 30;
+lDiff = L/(K+1);
+bDiff = B/(K/2+1);
+
+Lmeans = zeros(2,K);
+Lcovs = zeros(2,2,K);
+Bmeans = zeros(2,K);
+Bcovs = zeros(2,2,K);
+
+for k = 1:K/2
+    sb = [bDiff*k 0]'; % L bottom left corner    
+    Pb = 0.01*[0.1 0;
+               0   0.1];
+    Bmeans(:,k) = sb;
+    Bcovs(:,:,k) = Pb;
+    
+           
+    hold on;grid on
+    n = 100; % Number of grid points
+    phi = linspace(0,2*pi,100); % Create grid in interval [0,2*pi] 4
+    % Sigma ellipse
+    Col = ['r', 'm','g'];
+    
+    for j = 1:length(means)
+        for sig = 1:3
+            x = repmat(sb,1,n)+sig*sqrtm(Pb)*[cos(phi);sin(phi)]; % Equation(12) in HA1 document
+            plot(x(1,:),x(2,:),Col(sig),'LineWidth',1)            
+        end
+    end
+    
+end
+
+for k = 1:K
+    sl = [0 lDiff*k]'; % L bottom left corner    
+    Pl = 0.01*[0.1 0;
+               0   0.1];
+    
+    Lmeans(:,k) = sl;
+    Lcovs(:,:,k) = Pl; 
+    
+    hold on
+    n = 100; % Number of grid points
+    phi = linspace(0,2*pi,100); % Create grid in interval [0,2*pi] 4
+    % Sigma ellipse
+    Col = ['r', 'm','g'];
+    
+    for j = 1:length(means)
+        for sig = 1:3
+            x = repmat(sl,1,n)+sig*sqrtm(Pl)*[cos(phi);sin(phi)]; % Equation(12) in HA1 document
+            plot(x(1,:),x(2,:),Col(sig),'LineWidth',1)
+        end
+    end
+end
 
 X = [0 0 B B 0;
-     0 L L 0 0]';
-plot(X(:,1),X(:,2),'r','LineWidth',2)
-axis([-2 8 -2 8])
+    0 L L 0 0]';
+plot(X(:,1),X(:,2),'b','LineWidth',2)
+axis equal
+%axis([-2 8 -2 8])
 
 
-%% 
-N = 1500;
-Z = mvnrnd(ones(N,1),ones(N,N),1)';
-tic 
-prob = mvnpdf(Z,ones(N,1),diag([ones(1,N)]));
-toc
-%%
-prod = 1;
-tic 
-for j = 1:N
-    prod = prod * mvnpdf(Z(1),1,1);
+%% Try to rotate 
+
+phi = angle(4)%pi/4;
+
+R = [cos(phi) -sin(phi); sin(phi) cos(phi)];
+
+
+for k = 1:K
+    
+    Lmeans(:,k) = R*Lmeans(:,k) + [xc;yc];
+    %Lcovs(:,:,k) = R*Lcovs(:,:,k);
+    
+    Bmeans(:,k) = R*Bmeans(:,k) + [xc;yc];
+    %Bcovs(:,:,k) = R*Bcovs(:,:,k);
 end
-toc
+
+
+%%
+
+figure(f)
+hold on
+
+for k = 1:K
+
+    n = 100; % Number of grid points
+    phi = linspace(0,2*pi,100); % Create grid in interval [0,2*pi] 4
+    % Sigma ellipse
+    Col = ['r', 'm','c'];
+    
+    for j = 1:length(means)
+        for sig = 1:3
+            x = repmat(Lmeans(:,k),1,n)+sig*sqrtm(Lcovs(:,:,k))*[cos(phi);sin(phi)]; % Equation(12) in HA1 document
+            plot(x(1,:),x(2,:),Col(sig),'LineWidth',1)
+        end
+    end
+end
+
+
+for k = 1:K/2
+
+    hold on;grid on
+    n = 100; % Number of grid points
+    phi = linspace(0,2*pi,100); % Create grid in interval [0,2*pi] 4
+    % Sigma ellipse
+    Col = ['r', 'm','c'];
+    
+    for j = 1:length(means)
+        for sig = 1:3
+            x = repmat(Bmeans(:,k),1,n)+sig*sqrtm(Bcovs(:,:,k))*[cos(phi);sin(phi)]; % Equation(12) in HA1 document
+            plot(x(1,:),x(2,:),Col(sig),'LineWidth',1)            
+        end
+    end
+    
+end
+
+
+
+
+%%
+
