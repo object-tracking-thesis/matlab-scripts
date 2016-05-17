8dcf3fd100d26f44fa56e8faf782a70446d7e195
nilmagnum <leue@student.chalmers.se>
2016-05-17 10:05:33 +0200

    added measurement model, motion model and a birth component

--- EllipticTracking/bicycleExample.m
+++ EllipticTracking/bicycleExample.m
@@ -13,4 +13,33 @@ for i=start_seq:end_seq
     pause(0.5)
 end
 
-%% 
+%% simplify to 2d xy
+bicycleClusters_xy = cell(1,n);
+for i=start_seq:end_seq
+    bicycleClusters_xy{i} = bicycleClusters{i}(:,1:2);
+end
+
+%% motion and measurement model
+%constant velocity
+T = 0.1;
+F = [1 0 T 0; 0 1 0 T; 0 0 1 0; 0 0 0 1];
+q = 1;
+Q = [(q*T^3)/3  0           (q*T^2)/2   0;
+     0          (q*T^3)/3   0           (q*T^2)/2;
+     (q*T^2)/2  0           q*T         0;
+     0          (q*T^2)/2   0           q*T];
+
+H = [1 0 0 0;...
+     0 1 0 0];
+R = 0.00015*diag([1 1]);
+
+%% birth component
+mu = [mean(bicycleClusters_xy{start_seq}) 0 0]';
+P = 0.1*diag(ones(1,4));
+v = 7;
+V = diag([1 1]);
+weight = 1;
+index = 1;
+birth_comp = giwComp(mu, P, v, V, weight, index);
+
+%% prediction
--- EllipticTracking/giwComp.m
+++ EllipticTracking/giwComp.m
@@ -1,12 +1,12 @@
-function giw_comp = giwComp(mu,P,v,S,weight,index)
+function giw_comp = giwComp(mu,P,v,V,weight,index)
 %a gaussian inverse-wishart component
 %parametrized by:
 % mu    - mean of the gaussian
 % P     - covariance of the gaussian
 % v     - scalar degrees of freedom for the IW
-% S     - DxD positive definite matrix for the IW
+% V     - DxD positive definite matrix for the IW
 % weight- weight of the component
 % index - index of the component
 
-    giw_comp = struct('mu',mu,'P',P,'v',v,'S',S,'weight',weight,'index',index);
+    giw_comp = struct('mu',mu,'P',P,'v',v,'V',V,'weight',weight,'index',index);
 end
