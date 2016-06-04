ed60dc1caf3f67235161c6c880abd4c365ffc122
nilmagnum <leue@student.chalmers.se>
2016-06-04 11:12:48 +0200

    some tweaks to make the campus example work flawlessly

--- EllipPHDfilter.m
+++ EllipPHDfilter.m
@@ -4,8 +4,8 @@ classdef EllipPHDfilter < handle
         birth_ellipses = [];
         d = 2;
         ps = 0.98;
-        min_survival_weight = 0.0001;
-        min_merge_dist = 0.05;
+        min_survival_weight = 0.001;
+        min_merge_dist = 0.5;
         max_comps = 50;
         number_of_targets = 0;
         index = 1;
@@ -25,6 +25,8 @@ classdef EllipPHDfilter < handle
                 this.index = this.index+1;
                 this.birth_ellipses = [this.birth_ellipses birth_target];
             end 
+            this.ellipses = this.birth_ellipses(1:7);
+            this.birth_ellipses(1:7) = [];
         end
         
         % run predictions, for existing targets
@@ -46,22 +48,42 @@ classdef EllipPHDfilter < handle
         %contains the NN predictions for each of the measurements with 1
         %being clutter, 2 car, 3 cycle, 4 pedestrian, 5 pedestrian groups
         function update(this, meas)
+            length(this.ellipses)
+            ind = [];
+            for i = 1:length(meas)
+                isMeas = 0;
+                for j = 1:length(this.ellipses)                    
+                    isMeas = this.ellipses(j).gating(meas(i));
+                    if meas(i).type == 3 & isMeas
+                        this.ellipses(j).x
+                    end
+                    if isMeas
+                        break;
+                    end
+                end
+                %when the measurement was not in any gate              
+                if ~isMeas
+                    ind = [ind i];
+                end
+            end
+            meas(ind) = [];
+            
             %set pd dynamically depending on whether a measurement was
             %received inside the gate of that target and update the weight
-            ellipses_no_gating = [];
+            ellipses_no_meas = [];
             for i = 1:length(this.ellipses)
-                gating = 0;
+                hasMeas = 0;
                 for j = 1:length(meas)
-                    gating = this.ellipses(i).gating(meas(j));
-                    if gating
+                    hasMeas = this.ellipses(i).gating(meas(j));
+                    if hasMeas
                         break;
                     end
                 end
                 %when there was no measurement within the gate              
-                if ~gating
+                if ~hasMeas
                     this.ellipses(i).updateWeightNoGating;
                     ellipse_copy = this.ellipses(i).copy;
-                    ellipses_no_gating = [ellipses_no_gating ellipse_copy];                    
+                    ellipses_no_meas = [ellipses_no_meas ellipse_copy];                    
                 end
             end
             
@@ -82,8 +104,8 @@ classdef EllipPHDfilter < handle
             counter = 0;
             for i = 1:n_meas                
                 mantissas = [];
-                exponents = [];
-                for j = 1:n_pred                                        
+                exponents = [];                
+                for j = 1:n_pred
                     [mantissa, base10_exponent] = new_ellipses(j+((i-1)*n_pred)).calcLikelihood(meas(i));
                     
                     %the mantissa is updated with the old weight to keep
@@ -97,20 +119,21 @@ classdef EllipPHDfilter < handle
                 end
                 %normalizing the weights with the weightsum for the entire measurement  
                 new_ellipses(counter-n_pred+1:counter) = ...
-                    this.normalize_weights(new_ellipses(counter-n_pred+1:counter), exponents, mantissas);
-            end
+                    this.normalize_weights(new_ellipses(counter-n_pred+1:counter), exponents, mantissas);              
+            end            
             
             this.ellipses = new_ellipses;
             this.weight_sort_components;
-            this.prune;
+            this.prune;            
             
             %run the actual update on all new ellipses that actually
             %survived the pruning
             for i = 1:length(this.ellipses)
                 this.ellipses(i).update();
-            end            
-            
-            this.ellipses = [this.ellipses  ellipses_no_gating];
+            end        
+                      
+            this.ellipses = [this.ellipses  ellipses_no_meas];
+            this.prune;                             
             
             this.merge;
             %keep only the max_comps best components
@@ -128,11 +151,12 @@ classdef EllipPHDfilter < handle
             %now matlab can safely calculate the exponentials but we still
             %retain the relative difference between the high-weighted
             %components and the very low-weighted components are irrelevant
-            %anyways
+            %anyways            
             exponents_norm = exponents-repmat(min(exponents),1,length(exponents));
             exponents_norm = exponents_norm-repmat((max(exponents_norm)-50),1,length(exponents_norm));            
             exponents_norm(exponents_norm < -10) = -10;            
-            expe = exp(exponents_norm);            
+            expe = exp(exponents_norm); 
+            mantissas(mantissas == 0) = 0.1;
             weights = mantissas.*expe;
             norm_weights = weights./sum(weights);         
             for i = 1:length(ellipses)                    
@@ -156,7 +180,7 @@ classdef EllipPHDfilter < handle
             to_delete = [];
             for i=1:length(this.ellipses)
                 weightvec(i) = this.ellipses(i).weight;
-                if this.ellipses(i).weight < this.min_survival_weight
+                if this.ellipses(i).weight < this.min_survival_weight | abs(this.ellipses(i).x(5)) > 5
                     to_delete = [to_delete i];
                 end
             end
