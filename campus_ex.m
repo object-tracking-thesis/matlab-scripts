067cff48ec18da285c57e47b91c2916e590ba5b5
nilmagnum <leue@student.chalmers.se>
2016-06-05 08:45:32 +0200

    some tweaks to make the campus example work flawlessly

--- campus_ex.m
+++ campus_ex.m
@@ -6,22 +6,24 @@ start_seq = 1;
 end_seq = 186;
 
 %% plot the clusters
-figure
-for i=start_seq:end_seq
-    i
-    for j=1:length(clusters{i})
-        pcshow(pointCloud(clusters{i}{j}))
-        hold on
-        axis([-10 50 -50 20 -1 2])
-        zoom(2)
-    end
-    hold off
-    pause(0.3)
-end
+% figure
+% for i=start_seq:end_seq
+%     i
+%     for j=1:length(clusters{i})
+%         pcshow(pointCloud(clusters{i}{j}))
+%         hold on
+%         axis([-10 50 -50 20 -1 2])
+%         zoom(2)
+%     end
+%     hold off
+%     pause(0.3)
+% end
 
 %% create the measurement components
 meas = cell(1,n);
 carMeas = cell(1,n);
+% pedMeas = cell(1,n);
+% cycMeas = cell(1,n);
 for i=start_seq:end_seq    
     meas{i} = [];
     carMeas{i} = [];
@@ -31,20 +33,26 @@ for i=start_seq:end_seq
         elseif isCarMat(i,j) == 3 || isCarMat(i,j) == 4 || isCarMat(i,j) == 5
             meas{i} = [meas{i} giwMeasComp(clusters{i}{j}(:,1:3),isCarMat(i,j))];
         end
+%         elseif isCarMat(i,j) == 3
+%             cycMeas{i} = [cycMeas{i} giwMeasComp(clusters{i}{j}(:,1:3),isCarMat(i,j))];
+%         elseif isCarMat(i,j) == 4
+%             pedMeas{i} = [pedMeas{i} giwMeasComp(clusters{i}{j}(:,1:3),isCarMat(i,j))];
+%         end
     end
 end
 
 %% birth components
-ellipMeans = cell(1,1);
+ellipMeans = cell(1,9);
 ellipMeans{1} = [0 -20 2 2 0 0]';
 ellipMeans{2} = [-3 -16 2 2 0 0]';
 ellipMeans{3} = [8 10 -2 0 0 0]';
-ellipMeans{4} = [28 10 -2 0 0 0]';
-ellipMeans{5} = [22 10 2 0 0 0]';
-ellipMeans{6} = [18 9 -1 0 0 0]';
-ellipMeans{7} = [8 0 2 2 0 0]';
-ellipMeans{8} = [-10 -25 4 4 0 0]';
-ellipMeans{9} = [30 3 -6 0 0 0]';
+ellipMeans{4} = [7 10 -2 0 0 0]';
+ellipMeans{5} = [28 10 -2 0 0 0]';
+ellipMeans{6} = [22 10 2 0 0 0]';
+ellipMeans{7} = [18 9 -1 0 0 0]';
+ellipMeans{8} = [8 0 2 2 0 0]';
+ellipMeans{9} = [-10 -24 2 2 0 0]';
+ellipMeans{10} = [27 3 -3 0 0 0]';
 
 rectMeans = cell(1,1);
 rectMeans{1} = [15 0 4 0 0 1.8 4.7]';
@@ -52,15 +60,15 @@ rectMeans{1} = [15 0 4 0 0 1.8 4.7]';
 %% run filter recursion
 ellip_phd = EllipPHDfilter;
 ellip_phd.set_birth_rfs(ellipMeans);
-rect_phd = RectPHDfilter;
-rect_phd.set_birth_rfs(rectMeans);
+% rect_phd = RectPHDfilter;
+% rect_phd.set_birth_rfs(rectMeans);
 targets = [];
 for i=start_seq:end_seq   
     i
     figure(1);
-    for j=1:length(clusters_xy{i})
+    for j=1:length(meas{i})
         color = [0.9, 0.9, 0.9];
-        plot(clusters_xy{i}(j).points(1,:),clusters_xy{i}(j).points(2,:),'x','Color',color)
+        plot(meas{i}(j).points(1,:),meas{i}(j).points(2,:),'x','Color',color)
         axis([-10 50 -50 20])
         hold on
     end    
@@ -68,10 +76,10 @@ for i=start_seq:end_seq
     ellip_phd.update(meas{i});
     ellip_est = ellip_phd.get_best_estimates;
     
-    rect_phd.predict;    
-    rect_phd.update(carMeas{i});
-    rect_phd.get_number_of_targets
-    rect_est = rect_phd.get_best_estimates;
+%     rect_phd.predict;    
+%     rect_phd.update(carMeas{i});
+%     rect_phd.get_number_of_targets
+%     rect_est = rect_phd.get_best_estimates;
     
     %plot the target ellipse and the measured points
     for j=1:length(ellip_est)
@@ -84,14 +92,14 @@ for i=start_seq:end_seq
         testtxt = strcat('\leftarrow i: ', num2str(ellip_est(j).index), ', w: ', num2str(ellip_est(j).weight));
         text(double(mu(1)), double(mu(2)), testtxt)
     end
-    for j=1:length(rect_est)
-        [mu, P, v, V] = rect_est(j).getState();
-        drawMyRide(mu,'r')
-    end
+%     for j=1:length(rect_est)
+%         [mu, P, v, V] = rect_est(j).getState();
+%         drawMyRide(mu,'r')
+%     end
     text(25, 15, strcat('frame: ', num2str(i)))
     axis equal
     
     hold off
-%     pause(0.5)
-    waitforkey
+    pause(0.01)
+%     waitforkey
 end
